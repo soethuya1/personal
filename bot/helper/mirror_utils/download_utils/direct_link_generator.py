5f8bbc83c3eac3980afd8e00f4554b8b65b10433
dark night <85269316+darkbluenova@users.noreply.github.com>
2021-12-03 12:26:19 +0630

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -313,6 +313,7 @@ def streamtape(url: str) -> str:
 
 def streamlare(url: str) -> str:
     """ Streamlare direct link generator
+    try:
     link = re.findall(r'\bhttps?://.*streamlare\.com\S+', url)[0]
     except IndexError:
         raise DirectDownloadLinkException("No streamlare links found\n")
