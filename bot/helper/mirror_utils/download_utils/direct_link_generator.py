6188505ea32058816cdb6f72a9954e609596f662
dark night <85269316+darkbluenova@users.noreply.github.com>
2021-12-03 14:18:48 +0630

    Update direct_link_generator.py

--- bot/helper/mirror_utils/download_utils/direct_link_generator.py
+++ bot/helper/mirror_utils/download_utils/direct_link_generator.py
@@ -318,7 +318,7 @@ def streamlare(url: str) -> str:
     except IndexError:
         raise DirectDownloadLinkException("No streamlare links found\n")
     page = BeautifulSoup(requests.get(link).content, 'lxml')
-    info = page.find('p', {'id': 'downloadCollapse'})
+    info = page.find('a', {'href': 'larecontent.com'})
     return info.get('href')
 
 
