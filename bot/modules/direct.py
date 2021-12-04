0a26402aba9cc63294693fd91225e89aa5b75cbb
dark night <85269316+darkbluenova@users.noreply.github.com>
2021-12-04 14:40:40 +0630

    Update direct.py

--- bot/modules/direct.py
+++ bot/modules/direct.py
@@ -11,25 +11,18 @@ from bot.helper.telegram_helper.filters import CustomFilters
 from bot import dispatcher
 from bot.helper.telegram_helper.bot_commands import BotCommands
 from bot.helper.telegram_helper.message_utils import sendMessage, editMessage
-from bot.helper.ext_utils.exceptions import DirectDownloadLinkException
 from telegram.ext import CommandHandler  
 
 
 def direct(update, context):
-    elif 'ouo.press' in link:
-        return ouo(link)
-    elif 'ouo.io' in link:
-        return ouo(link)
-    else:
-        raise DirectDownloadLinkException(f'No Direct link function found for {link}')
-
-def ouo(url: str) -> str:
-    """ Bypass OuO
-    Based on https://github.com/zevtyardt/lk21
-    """
+    if 'ouo.press' in link
+  try:
     bypasser = lk21.Bypass()
     dl_url=bypasser.bypass_ouo(url)
-    return sendMessage(dl_url)
+    await update.reply_text('{dl_url}')
+    else:
+    return
+    await update.reply_text('No ouo link found')
 
 
 
